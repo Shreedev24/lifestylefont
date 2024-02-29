8138fe3e8c6291be1512ffc3b6f88315a39041cf
Shreedev <shreedevezhava@gmail.com>
2024-02-29 11:16:46 +0530

    eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import { Form } from 'react-bootstrap';
+// import Jumbotron from 'react-bootstrap/Jumbotron';
+// import Toast from 'react-bootstrap/Toast';
+// import Container from 'react-bootstrap/Container';
+// import Button from 'react-bootstrap/Button';
+// import { Form } from 'react-bootstrap';
 import Header from './Header';
 import ImageGrid from './ImageGrid';
 import './App.css';
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
    <div>
-   <Header/>
+   <Header/> 
    <ImageGrid/>
    </div>
     )
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
+import React from 'react';
 import roomdata from './roomdata.json';
 
 function Header() {
