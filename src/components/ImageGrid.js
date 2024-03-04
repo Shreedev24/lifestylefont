8ba360c358e38013211e1c1ac60216b5b83a5826
jiyan <jiyanprajapati1@gmail.com>
2024-03-04 17:26:08 +0530

    push data

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,50 +1,3 @@
-// import React from 'react';
-
-// function ImageGrid() {
-//   return (
-//     <div className="container mt-3">
-//       <div className="row">
-//       <div className="col-md-12">
-//       <hr />
-//       </div>
-//       <div className="row">
-//       <div className="col-md-12 py-3">
-      
-//         <div className='pl-3'>Results: 3</div>
-        
-//       </div>
-//       </div>
-
-//       </div>
-//       <div className="row justify-content-center">
-//         <div className="col-md-4 mb-3 d-flex justify-content-center">
-//           <img
-//             src="1.jpg"
-//             alt="Image 1"
-//             className="img-fluid"
-//           />
-//         </div>
-
-//         <div className="col-md-4 mb-3 d-flex justify-content-center">
-//           <img
-//             src="2.jpg"
-//             alt="Image 2"
-//             className="img-fluid"
-//           />
-//         </div>
-
-//         <div className="col-md-4 mb-3 d-flex justify-content-center">
-//           <img
-//             src="3.jpg"
-//             alt="Image 3"
-//             className="img-fluid"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 // export default ImageGrid;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -76,12 +29,16 @@ export default function ImageGrid() {
       <div className="row justify-content-left">
         {(
           imageNames.map((data, index) => (
-            <div key={index} className="col-md-4 mb-3 d-flex justify-content-center">
-              <img
+            <div key={index} className="col-md-4 mb-3 flex-container column justify-content-center">
+               <div className='d-block'> <img
                 src={`https://backendlifestyle.netlify.app/images/${data.image}`} 
                 alt={`Image ${index + 1}`}
                 className="img-fluid"
-              />
+              /></div>
+              <div className=''>{data.product}</div>
+              <div className=''>{data.roomtype}</div>
+              <div className=''>{data.color}</div>
+              <div className=''>{data.roomLight}</div>
             </div>
           ))
         )}
